f0a6130df32d6dfa59137b6893e4fad0c81c849a
harshgarg0906 <47844943+harshgarg0906@users.noreply.github.com>
2019-12-21 23:45:24 +0530

    Add files via upload

---
+++ accountmanagment/src/test/java/com/example/demo/AccountmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
